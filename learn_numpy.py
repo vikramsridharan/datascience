c841268818b8e425de5481317b10e8e7ce777e3a
Vikram Sridharan <sridharan.vikram@gmail.com>
2021-04-23 11:39:24 +0530

    numpy import and config

--- learn_numpy.py
+++ learn_numpy.py
@@ -1,6 +1,11 @@
-print("hello!")
+import numpy as np
 
-a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(a[9::-1])
+#print(np.__version__)
+#np.show_config()
 
-print("Check")
+#print("hello!")
+
+#a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#print(a[9::-1])
+
+#print("Check")
